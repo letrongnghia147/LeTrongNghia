11219fa9cbdc520166c738394ff3fc74057c9701
nghia le <letrongnghia147@gmail.com>
2021-03-19 10:30:42 +0700

    hih

---
+++ hii/hii/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace hii
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
